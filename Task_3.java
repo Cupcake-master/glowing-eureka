acc3c2588720d1dfe7565a52acdf7bcbab099a74
bbilalov <bulat.bilalov@orioninc.com>
2023-02-24 21:03:00 +0100

    Added Task_3.java

---
+++ Task_3.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        int[] ints = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();
+        findMaxIncreasingSubarray(ints);
+    }
+
+    public static void findMaxIncreasingSubarray(int[] arr) {
+        int maxLength = 1; // максимальная длина подмассива, начинающегося с первого элемента
+        int currentLength = 1; // длина текущего подмассива
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                currentLength++;
+                maxLength = Math.max(maxLength, currentLength);
+            } else {
+                currentLength = 1;
+            }
+        }
+
+        System.out.println(maxLength);
+    }
+
+
+}
